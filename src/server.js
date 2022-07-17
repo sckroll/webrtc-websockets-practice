241c6ddfe664b582917f20884809a9ecc91a0fe5
김성찬 <kimsc0714@naver.com>
2022-07-17 23:57:59 +0900

    feat: 기본적인 서버 설정 및 프론트엔드 디렉토리 설정

--- src/server.js
+++ src/server.js
@@ -2,5 +2,18 @@ import express from 'express';
 
 const app = express();
 
-console.log('hello');
+// 뷰 엔진을 Pug로 설정 & 뷰 파일이 위치한 디렉토리를 설정
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// static 디렉토리 설정
+app.use('/public', express.static(__dirname + '/public'));
+
+// 라우트 설정 (home.pug 렌더링 수행)
+app.get('/', (req, res) => res.render('home'));
+
+// 다른 라우트를 입력했을 경우 루트 경로로 리다이렉트
+app.get('/*', (req, res) => res.redirect('/'));
+
+// 3000 포트로 서버 실행
 app.listen(3000);
