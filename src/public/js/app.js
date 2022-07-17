a30393b4120591090915b8e54561376d79345e90
김성찬 <kimsc0714@naver.com>
2022-07-18 02:54:45 +0900

    feat: ws 모듈로 서버와 클라이언트 간 채팅 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,54 @@
-console.log('asdf');
+// DOM 요소 받기
+const msgList = document.querySelector('ul');
+const nickForm = document.querySelector('#nick');
+const msgForm = document.querySelector('#message');
+
+// 메시지 타입과 값을 객체로 만들고 문자열로 만드는 함수
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+// 웹소켓 연결
+// 클라이언트에서의 socket은 서버로의 연결을 의미
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// socket이 열렸을 때 발생하는 이벤트
+socket.addEventListener('open', () => {
+  console.log('Connected to Server');
+});
+
+// 서버로부터 메시지를 받았을 때 발생하는 이벤트
+socket.addEventListener('message', message => {
+  // 메시지를 화면에 렌더링
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  msgList.append(li);
+});
+
+// socket 연결이 끊겼을 때 발생하는 이벤트
+socket.addEventListener('close', () => {
+  console.log('Disconnected from Server');
+});
+
+// 일정 시간 후 메시지 보내기
+// setTimeout(() => {
+//   socket.send('hello from the browser');
+// }, 3000);
+
+// 메시지 form 이벤트 처리 & 서버로 메시지 보내기
+msgForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const input = msgForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+})
+
+// 닉네임 form 이벤트 처리
+nickForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+});
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -15,5 +17,55 @@ app.get('/', (req, res) => res.render('home'));
 // 다른 라우트를 입력했을 경우 루트 경로로 리다이렉트
 app.get('/*', (req, res) => res.redirect('/'));
 
+const handleListen = () => console.log('Listening to http://localhost:3000');
+
 // 3000 포트로 서버 실행
-app.listen(3000);
+// app.listen(3000, handleListen);
+
+// HTTP & 웹소켓(ws) 서버 동시에 돌리기
+// 이로써 같은 localhost:3000 주소에서 HTTP와 웹소켓 요청을 동시에 처리 가능 
+// (WS만 연결시킬 수 있음, 즉 HTTP는 필수가 아님)
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// 임시 데이터베이스
+const sockets = [];
+
+// connection: 연결되었을 때 발생하는 이벤트
+// 서버에서의 socket은 연결된 브라우저를 의미
+wss.on('connection', socket => {
+  // 임시 DB에 socket 추가
+  sockets.push(socket);
+
+  // 익명의 경우 socket 닉네임 속성을 Ananymous로 설정
+  socket['nickname'] = 'Anonymous';
+
+  // 브라우저와 연결이 끊겼을 때 발생하는 이벤트
+  socket.on('close', () => {
+    console.log('Disconnected from Browser');
+  })
+
+  // 브라우저로부터 메시지를 받았을 경우 발생하는 이벤트
+  socket.on('message', msg => {
+    // 받은 메시지를 객체로 변환
+    const message = JSON.parse(msg);
+
+    switch (message.type) {
+      case 'new_message':
+        // 연결된 각 브라우저에 메시지 보내기
+        sockets.forEach(aSocket => {
+          aSocket.send(`${socket.nickname}: ${message.payload}`);
+        });
+        break;
+      case 'nickname':
+        socket['nickname'] = message.payload;
+        break;
+    }
+
+  });
+
+  console.log('Connected to Browser');
+  socket.send('hello!');
+});
+
+server.listen(3000, handleListen);
