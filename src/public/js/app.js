631e030697902de6812718937fe0d58333dd5789
김성찬 <kimsc0714@naver.com>
2022-07-20 01:11:27 +0900

    feat: 채팅 기본 기능, 닉네임, 전체 메시지 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -9,12 +9,51 @@ room.hidden = true;
 
 let roomName = '';
 
+// 메시지를 화면에 표시
+function addMessage(message) {
+  const ul = room.querySelector('ul');
+  const li = document.createElement('li');
+
+  li.innetText = message;
+  ul.appendChild(li);
+}
+
+// 메시지 보내기
+function handleMessageSubmit(e) {
+  e.preventDefault();
+
+  const input = room.querySelector('#msg input');
+  const value = input.value;
+
+  socket.emit('new_message', input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = '';
+}
+
+// 닉네임 변경
+function handleNicknameSubmit(e) {
+  e.preventDefault();
+
+  const input = room.querySelector('#name input');
+  const value = input.value;
+
+  socket.emit('nickname', input.value);
+  input.value = '';
+}
+
+// 닉네임 입력 폼과 채팅 촘 전환
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
 
   const h3 = room.querySelector('h3');
   h3.innerText = `Room ${roomName}`;
+
+  const msgForm = room.querySelector('#msg');
+  const nameForm = room.querySelector('#name');
+  msgForm.addEventListener('submit', handleMessageSubmit);
+  nameForm.addEventListener('submit', handleNicknameSubmit);
 }
 
 form.addEventListener('submit', e => {
@@ -34,3 +73,33 @@ form.addEventListener('submit', e => {
   roomName = input.value;
   input.valut = '';
 })
+
+socket.on('welcome', (user, newCount) => {
+  const h3 = room.querySelector('h3');
+  h3.innerText = `Room ${roomName} (${newCount})`;
+
+  addMessage(`${user} joined!`);
+});
+
+socket.on('bye', (left, newCount) => {
+  const h3 = room.querySelector('h3');
+  h3.innerText = `Room ${roomName} (${newCount})`;
+
+  addMessage(`${left} left ㅜㅜ!`);
+});
+
+// socket.on('new_message', msg => addMessage(msg));
+socket.on('new_message', addMessage);
+
+// 방 개수 업데이터
+socket.on('room_change', rooms => {
+  const roomList = welcome.querySelector('ul');
+  roomList.innerHTML = '';
+  if (room.length === 0) return;
+
+  rooms.forEach(room => {
+    const li = document.createElement('li');
+    li.innerText = room;
+    roomList.append(li);
+  });
+});
--- src/server.js
+++ src/server.js
@@ -24,10 +24,49 @@ app.get('/*', (req, res) => res.redirect('/'));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+// public rooms를 반환하는 함수
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    }
+  } = wsServer;
+
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  })
+  return publicRooms;
+}
+
+// 사용자 카운트
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.room.get(roomName)?.size;
+}
+
 // 각 socket은 socket.id로 구분됨
 wsServer.on('connection', socket => {
+  // 익명의 닉네임으로 초기화
+  socket['nickname'] = 'Anonymous';
+
   // 호출된 이벤트를 콘솔에 출력
   socket.onAny(e => {
+    // Adapter 로그 출력
+    /*
+      Adapter?
+      다른 서버들 사이에 실시간 애플리케이션을 동기화
+      서버의 메모리에서 Adapter 사용 중
+      현재는 서버 종료 시 모든 room, message, socket은 사라짐
+      모든 클라이언트에 대해 connection을 연결해야 함 => 실시간으로 서버 메모리에 있어야 함
+      => 즉, 항상 오픈 상태를 유지
+      서버에 많은 connection이 들어옴
+      많은 connection을 메모리에 저장하게 됨 => 서버 여러 개를 사용
+      Adapter를 사용하겠다는 의미 = 같은 memory pool을 공유
+     */
+    console.log(wsServer.sockets.adapter);
+
     console.log(`Socket event: ${e}`);
   });
 
@@ -50,50 +89,43 @@ wsServer.on('connection', socket => {
     // 방 나가기
     // socket.leave(roomName);
 
-    // 방 전체에 이벤트 보내기
-    // socket.to(roomName).emit('custom_event', test);
+    // 특정한 방에 이벤트 보내기
+    // to()를 체이닝하거나, 인자로 배열을 보내면 여러 방에 이벤트를 보낼 수 있음
+    // 인자에 방 이름 대신 특정 socket.id를 주면 개인에게 이벤트 전달 가능 (ex: 개인 메시지)
+    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
+
+    // 연결된 모든 socket에 매시지 전송
+    wsServer.sockets.emit('room_change', publicRooms());
   });
+
+  // 접속 중단 시 발생하는 이벤트 (아직 완전히 나가기 전)
+  // 완전히 끊어졌을 때의 disconnect와 구별할 것
+  socket.on('disconnecting', () => {
+    // rooms: 존재하는 모든 방을 담은 Set
+    socket.rooms.forEach(room => socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1));
+  })
+  
+  socket.on('disconnect', () => {
+    // 연결된 모든 socket에 매시지 전송
+    wsServer.sockets.emit('room_change', publicRooms());
+  })
+
+  // 메시지 수신
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
+    done();
+  })
+
+  // 닉네임 변경
+  socket.on('nickname', nickname => {
+    socket['nickname'] = nickname;
+  });
+
+  // 사용자 한 명을 만들어거 강제로 socket이 방에 입장하도록 하는 방법
+  // wsServer.socketsJoin('room1');
+  // 혹은 특정 방에 있는 모든 socket을 다른 방으로 들어가게 할 수 있음
+  // wsServer.in('room1').socketsJoin(['room2', 'room3']);
 });
-
-// // 임시 데이터베이스
-// const sockets = [];
-
-// // connection: 연결되었을 때 발생하는 이벤트
-// // 서버에서의 socket은 연결된 브라우저를 의미
-// wss.on('connection', socket => {
-//   // 임시 DB에 socket 추가
-//   sockets.push(socket);
-
-//   // 익명의 경우 socket 닉네임 속성을 Ananymous로 설정
-//   socket['nickname'] = 'Anonymous';
-
-//   // 브라우저와 연결이 끊겼을 때 발생하는 이벤트
-//   socket.on('close', () => {
-//     console.log('Disconnected from Browser');
-//   })
-
-//   // 브라우저로부터 메시지를 받았을 경우 발생하는 이벤트
-//   socket.on('message', msg => {
-//     // 받은 메시지를 객체로 변환
-//     const message = JSON.parse(msg);
-
-//     switch (message.type) {
-//       case 'new_message':
-//         // 연결된 각 브라우저에 메시지 보내기
-//         sockets.forEach(aSocket => {
-//           aSocket.send(`${socket.nickname}: ${message.payload}`);
-//         });
-//         break;
-//       case 'nickname':
-//         socket['nickname'] = message.payload;
-//         break;
-//     }
-
-//   });
-
-//   console.log('Connected to Browser');
-//   socket.send('hello!');
-// });
 
 const handleListen = () => console.log('Listening to http://localhost:3000');
 httpServer.listen(3000, handleListen);
